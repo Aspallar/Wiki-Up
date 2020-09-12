9a366321a8e1e3d8ca3e787accc55f35720c464f
Aspallar <shadowthrone99@outlook.com>
2020-09-12 23:42:47 +0100

    AddCategory button added

--- src/WikiUpload/Pages/UploadPage.xaml.cs
+++ src/WikiUpload/Pages/UploadPage.xaml.cs
@@ -9,5 +9,14 @@
         {
             InitializeComponent();
         }
+
+        private void AddCategory_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            const string enterCategory = "Enter Category Name";
+            ContentText.Text += $"\n[[Category:{enterCategory}]]";
+            ContentText.Select(ContentText.Text.Length - enterCategory.Length - 2, enterCategory.Length);
+            ContentText.ScrollToEnd();
+            ContentText.Focus();
+        }
     }
 }
