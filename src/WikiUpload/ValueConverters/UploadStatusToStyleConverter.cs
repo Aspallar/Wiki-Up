d15b6ff64bbf97975a6151fe66481d43f2a3d26d
Aspallar <shadowthrone99@outlook.com>
2020-09-12 01:26:52 +0100

    Added upload status icons

---
+++ src/WikiUpload/ValueConverters/UploadStatusToStyleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace WikiUpload
+{
+    internal class UploadStatusToStyleConverter : BaseValueConverter<UploadStatusToStyleConverter>
+    {
+        private readonly Style _spin;
+        private readonly Style _fontsize;
+
+        public UploadStatusToStyleConverter()
+        {
+            _spin = (Style)Application.Current.FindResource("SpinningIcon");
+            _fontsize = (Style)Application.Current.FindResource("FontSizeIcon");
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (UploadFileStatus)value == UploadFileStatus.Uploading ? _spin : _fontsize;
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
