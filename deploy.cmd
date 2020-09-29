dbf4ca434367bf8a22a6ae35e38a3f0acd2d14de
Aspallar <shadowthrone99@outlook.com>
2020-09-29 19:31:22 +0100

    Add ToggleSwitch to deploy

--- deploy.cmd
+++ deploy.cmd
@@ -8,7 +8,7 @@ copy src\WikiUpload\bin\release\WikiUp.exe.config Deploy\.
 copy src\WikiUpload\bin\release\Microsoft.Windows.Shell.dll Deploy\.
 copy src\WikiUpload\bin\release\MahApps.Metro.IconPacks.Core.dll Deploy\.
 copy src\WikiUpload\bin\release\MahApps.Metro.IconPacks.FontAwesome.dll Deploy\.
-
+copy src\WikiUpload\bin\release\ToggleSwitch.dll Deploy\.
 goto :eof
 
 :wrongfolder
