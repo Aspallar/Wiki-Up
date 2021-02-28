852d8de57e0de7edce0187f5a0763ab7188bc4b1
Aspallar <shadowthrone99@outlook.com>
2021-02-28 16:23:18 +0000

    add dependencies for videos to deplotment

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 if not exist .git\ goto :wrongfolder 
 if not exist src\ goto :wrongfolder 
 md Deploy 1>nul 2>nul
@@ -7,15 +8,26 @@ md Deploy\et-EE 1>nul 2>nul
 del Deploy\*.* /q
 del Deploy\de-DE\*.* /q
 del Deploy\et-EE\*.* /q
-copy src\WikiUpload\bin\release\WikiUp.exe Deploy\.
-copy src\WikiUpload\bin\release\WikiUp.exe.config Deploy\.
-copy src\WikiUpload\bin\release\Microsoft.Windows.Shell.dll Deploy\.
-copy src\WikiUpload\bin\release\MahApps.Metro.IconPacks.Core.dll Deploy\.
-copy src\WikiUpload\bin\release\MahApps.Metro.IconPacks.FontAwesome.dll Deploy\.
-copy src\WikiUpload\bin\release\Ninject.dll Deploy\.
-copy src\WikiUpload\bin\release\ToggleSwitch.dll Deploy\.
-copy src\WikiUpload\bin\release\de-DE\WikiUp.resources.dll Deploy\de-DE\.
-copy src\WikiUpload\bin\release\et-EE\WikiUp.resources.dll Deploy\et-EE\.
+set src=src\WikiUpload\bin\release
+copy %src%\WikiUp.exe Deploy\.
+copy %src%\WikiUp.exe.config Deploy\.
+copy %src%\Microsoft.Windows.Shell.dll Deploy\.
+copy %src%\MahApps.Metro.IconPacks.Core.dll Deploy\.
+copy %src%\MahApps.Metro.IconPacks.FontAwesome.dll Deploy\.
+copy %src%\Ninject.dll Deploy\.
+copy %src%\ToggleSwitch.dll Deploy\.
+copy %src%\AngleSharp.dll Deploy\.
+copy %src%\System.Runtime.CompilerServices.Unsafe.dll Deploy\.
+copy %src%\System.Text.Encoding.CodePages.dll Deploy\.
+copy %src%\Google.Apis.Auth.dll Deploy\.
+copy %src%\Google.Apis.Auth.PlatformServices.dll Deploy\.
+copy %src%\Google.Apis.Core.dll Deploy\.
+copy %src%\Google.Apis.dll Deploy\.
+copy %src%\Google.Apis.PlatformServices.dll Deploy\.
+copy %src%\Google.Apis.YouTube.v3.dll Deploy\.
+copy %src%\Newtonsoft.Json.dll Deploy\.
+copy %src%\de-DE\WikiUp.resources.dll Deploy\de-DE\.
+copy %src%\et-EE\WikiUp.resources.dll Deploy\et-EE\.
 
 goto :eof
 
