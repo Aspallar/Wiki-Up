044a25972efa10d9b01d74cf00e88afce01f98b6
Aspallar <shadowthrone99@outlook.com>
2021-03-06 15:45:03 +0000

    Set focus when file extension popup closed

---
+++ src/WikiUpload/AttachedProperties/PopupClosedFocusProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace WikiUpload
+{
+    public class PopupClosedFocusProperty : BaseAttachedProperty<PopupClosedFocusProperty, object>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != e.OldValue && sender is Popup popup)
+                popup.Closed += Popup_Closed;
+        }
+
+        private void Popup_Closed(object sender, EventArgs e)
+        {
+            if (sender is DependencyObject d && d.GetValue(PopupClosedFocusProperty.ValueProperty) is Control control)
+                control.Focus();
+        }
+    }
+}
