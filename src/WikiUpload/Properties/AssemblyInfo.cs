ea5c4c77a842a9daf281b3a47f72109a75763178
Aspallar <shadowthrone99@outlook.com>
2021-03-10 19:59:30 +0000

    Update copyright years

--- src/WikiUpload/Properties/AssemblyInfo.cs
+++ src/WikiUpload/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aspallar")]
 [assembly: AssemblyProduct("Wiki-Up")]
-[assembly: AssemblyCopyright("Copyright © David Marshall (aka Aspallar) 2019-2020")]
+[assembly: AssemblyCopyright("Copyright © David Marshall (aka Aspallar) 2019-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
