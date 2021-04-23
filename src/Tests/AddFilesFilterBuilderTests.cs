0f538a5194b8b12a89c304c494838a0e34ccdad7
Aspallar <shadowthrone99@outlook.com>
2021-04-23 14:55:48 +0100

    Add tests for add files filter
    and fix the bug they revealed by test When_NoPermittedFilesAndNoImages_Then_FilterDoesNotContainImageEntry

---
+++ src/Tests/AddFilesFilterBuilderTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using WikiUpload;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AddFilesFilterBuilderTests
+    {
+        [Test]
+        public void When_NoPermittedFiles_Then_FilterIsForAllFiles()
+        {
+            var expected = "Image Files|*.foo;*.bar|Other Files|*.odt;*.ods;*.odp;*.odg;*.odc;*.odf;*.odi;*.odm;*.ogg;*.ogv;*.oga|All Files|*.*";
+
+            var result = AddFilesFilterBuilder.Build(Array.Empty<string>(), "foo;bar");
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void When_NoPermittedFilesAndNoImages_Then_FilterDoesNotContainImageEntry()
+        {
+            var expected = "Other Files|*.odt;*.ods;*.odp;*.odg;*.odc;*.odf;*.odi;*.odm;*.ogg;*.ogv;*.oga|All Files|*.*";
+
+            var result = AddFilesFilterBuilder.Build(Array.Empty<string>(), "");
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void When_NoImages_Then_FilterDoesNotContainImageEntry()
+        {
+            var expected = "Other Files|*.odt|All Files|*.*";
+
+            var result = AddFilesFilterBuilder.Build(new string[] { ".odt" }, "");
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void When_AllPermittedFilesAreImages_Then_FilterDoesNotContainOtherFileEntry()
+        {
+            var expected = "Image Files|*.foo;*.bar|All Files|*.*";
+
+            var result = AddFilesFilterBuilder.Build(new string[] { ".foo", ".bar" }, "foo;bar");
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void When_PermittedFiles_Then_OnlyPermittedFilesAreInFilter()
+        {
+            var expected = "Image Files|*.foo|Other Files|*.foobar|All Files|*.*";
+
+            var result = AddFilesFilterBuilder.Build(new string[] { ".foo", ".foobar" }, "foo;bar" );
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+    }
+}
---
+++ src/WikiUpload/Utilities/AddFilesFilterBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace WikiUpload
+{
+    public static class AddFilesFilterBuilder
+    {
+        public static string Build(string[] permittedExtensions, string imageExtensionsString)
+        {
+            const string othersPrefix = "|Other Files|*";
+            const string imagesPrefix = "|Image Files|*";
+
+            var imageExtensions = imageExtensionsString.Split(';', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => '.' + x).ToList();
+            string images, others;
+
+            if (permittedExtensions.Length == 0)
+            {
+                others = othersPrefix + ".odt;*.ods;*.odp;*.odg;*.odc;*.odf;*.odi;*.odm;*.ogg;*.ogv;*.oga";
+                images = imageExtensions.Count > 0 ? imagesPrefix + string.Join(";*", imageExtensions) : string.Empty;
+            }
+            else
+            {
+                var imageFiles = permittedExtensions.Intersect(imageExtensions).ToList();
+                others = string.Join(";*", permittedExtensions.Except(imageFiles));
+                if (others.Length > 0)
+                    others = othersPrefix + others;
+                images = string.Join(";*", imageFiles);
+                if (images.Length > 0)
+                    images = imagesPrefix + images;
+            }
+
+            return $"{images}{others}|All Files|*.*"[1..];
+        }
+    }
+}
