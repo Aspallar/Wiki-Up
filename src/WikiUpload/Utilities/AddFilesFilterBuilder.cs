afb29b3a395f441b339062ab888603d06dc824d8
Aspallar <shadowthrone99@outlook.com>
2021-05-20 18:20:05 +0100

    Fix add files dialog for case where all image extensions have been removed in settings

---
+++ src/WikiUpload/Utilities/AddFilesFilterBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace WikiUpload
+{
+    public static class AddFilesFilterBuilder
+    {
+        private static char[] _separator = new char[] { ';' };
+
+        public static string Build(string[] permittedExtensions, string imageExtensionsString)
+        {
+            const string othersPrefix = "|Other Files|*";
+            const string imagesPrefix = "|Image Files|*";
+
+            var imageExtensions = imageExtensionsString.Split(_separator, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => '.' + x).ToList();
+            string images, others;
+
+            if (permittedExtensions.Length == 0)
+            {
+                others = othersPrefix + ".odt;*.ods;*.odp;*.odg;*.odc;*.odf;*.odi;*.odm;*.ogg;*.ogv;*.oga";
+                images = imageExtensions.Count > 0 ? imagesPrefix + string.Join(";*", imageExtensions) : string.Empty;
+            }
+            else
+            {
+                var imageFiles = permittedExtensions.Intersect(imageExtensions).ToList();
+                others = string.Join(";*", permittedExtensions.Except(imageFiles));
+                if (others.Length > 0)
+                    others = othersPrefix + others;
+                images = string.Join(";*", imageFiles);
+                if (images.Length > 0)
+                    images = imagesPrefix + images;
+            }
+
+            return $"{images}{others}|All Files|*.*".Substring(1);
+        }
+    }
+}
--- src/WikiUpload/Utilities/DialogManager.cs
+++ src/WikiUpload/Utilities/DialogManager.cs
@@ -16,39 +16,11 @@ namespace WikiUpload
                 Multiselect = true,
                 CheckPathExists = true,
                 CheckFileExists = true,
-                Filter = AddFilesFilter(permittedExtensions, imageExtensions),
+                Filter = AddFilesFilterBuilder.Build(permittedExtensions, imageExtensions)
             };
             var result = (bool)openFileDialog.ShowDialog();
             fileNames = openFileDialog.FileNames.ToList();
             return result;
-        }
-
-
-        private string AddFilesFilter(string[] permittedExtensions, string imageExtensionsString)
-        {
-            const string othersPrefix = "|Other Files|*";
-            const string imagesPrefix = "|Image Files|*";
-
-            var imageExtensions = imageExtensionsString.Split(';').Select(x => '.' + x).ToList();
-            string images, others;
-
-            if (permittedExtensions.Length == 0)
-            {
-                others = othersPrefix + ".odt;*.ods;*.odp;*.odg;*.odc;*.odf;*.odi;*.odm;*.ogg;*.ogv;*.oga";
-                images = imagesPrefix + string.Join(";*", imageExtensions);
-            }
-            else
-            {
-                var imageFiles = permittedExtensions.Intersect(imageExtensions).ToList();
-                others = string.Join(";*", permittedExtensions.Except(imageFiles));
-                if (others.Length > 0)
-                    others = othersPrefix + others;
-                images = string.Join(";*", imageFiles);
-                if (images.Length > 0)
-                    images = imagesPrefix + images;
-            }
-
-            return $"{images}{others}|All Files|*.*".Substring(1);
         }
 
         public bool LoadContentDialog(out string fileName)
