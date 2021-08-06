e0423f80c9e6baa0a534f035f1f75cc59b1e97ab
Aspallar <shadowthrone99@outlook.com>
2021-08-06 01:23:16 +0100

    Simplify FocusedPopup

--- src/WikiUpload/wpf/Controls/FocusedPopup.cs
+++ src/WikiUpload/wpf/Controls/FocusedPopup.cs
@@ -8,6 +8,35 @@ namespace WikiUpload
 {
     internal class FocusedPopup : Popup
     {
+        public FocusedPopup() : base()
+        {
+            KeyDown += Popup_KeyDown;
+            Opened += Popup_Opened;
+            Closed += Popup_Closed;
+        }
+
+        private void Popup_Closed(object sender, EventArgs e)
+        {
+            ExitFocus?.Focus();
+        }
+
+        private void Popup_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                IsOpen = false;
+        }
+
+        private void Popup_Opened(object sender, EventArgs e)
+        {
+            var initialFocus = InitialFocus;
+            if (initialFocus != null)
+            {
+                initialFocus.Focus();
+                if (initialFocus is TextBox tb)
+                    tb.SelectAll();
+            }
+        }
+
         #region InitiaFocus property
 
         public Control InitialFocus
@@ -20,43 +49,7 @@ namespace WikiUpload
             DependencyProperty.Register(
                 nameof(InitialFocus),
                 typeof(Control),
-                typeof(FocusedPopup),
-                new UIPropertyMetadata(new PropertyChangedCallback(OnInitialFocusChanged)));
-
-        private static void OnInitialFocusChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var popup = (Popup)sender;
-
-            if (e.NewValue != null)
-            {
-                if (e.OldValue == null)
-                {
-                    popup.Opened += Popup_Opened;
-                    popup.KeyDown += Popup_KeyDown;
-                }
-            }
-            else
-            {
-                popup.Opened -= Popup_Opened;
-                popup.KeyDown -= Popup_KeyDown;
-            }
-        }
-
-        private static void Popup_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-                ((Popup)sender).IsOpen = false;
-        }
-
-        private static void Popup_Opened(object sender, EventArgs e)
-        {
-            if (sender is DependencyObject d && d.GetValue(InitialFocusProperty) is Control control)
-            {
-                control.Focus();
-                if (control is TextBox tb)
-                    tb.SelectAll();
-            }
-        }
+                typeof(FocusedPopup));
 
         #endregion
 
@@ -72,29 +65,7 @@ namespace WikiUpload
             DependencyProperty.Register(
                 nameof(ExitFocus),
                 typeof(Control),
-                typeof(FocusedPopup),
-                new UIPropertyMetadata(new PropertyChangedCallback(OnExitFocusChanged)));
-
-        private static void OnExitFocusChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var popup = (Popup)sender;
-
-            if (e.NewValue != null)
-            {
-                if (e.OldValue == null)
-                    popup.Closed += Popup_Closed;
-            }
-            else
-            {
-                popup.Closed -= Popup_Closed;
-            }
-        }
-
-        private static void Popup_Closed(object sender, EventArgs e)
-        {
-            if (sender is DependencyObject d && d.GetValue(ExitFocusProperty) is Control control)
-                control.Focus();
-        }
+                typeof(FocusedPopup));
 
         #endregion
 
