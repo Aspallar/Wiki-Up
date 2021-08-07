d93466405116c13161ecaefac7533e8ded3c232a
Aspallar <shadowthrone99@outlook.com>
2021-08-07 18:12:23 +0100

    Implemented DeleteListBox

---
+++ src/WikiUpload/wpf/Controls/DeleteListBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WikiUpload
+{
+    internal class DeleteListBox : ListBox
+    {
+        public DeleteListBox() : base()
+        {
+            KeyDown += DeleteListBox_KeyDown;
+        }
+
+        private void DeleteListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+                DeleteSelectedCommand?.Execute(SelectedItems);
+        }
+
+        public ICommand DeleteSelectedCommand
+        {
+            get { return (ICommand)GetValue(DeleteSelectedCommandProperty); }
+            set { SetValue(DeleteSelectedCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeleteSelectedCommandProperty =
+            DependencyProperty.Register(
+                nameof(DeleteSelectedCommand),
+                typeof(ICommand),
+                typeof(DeleteListBox));
+    }
+}
