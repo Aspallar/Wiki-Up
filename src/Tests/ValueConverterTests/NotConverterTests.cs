e98e4bbf9becf37ced1dc64ed88d2a989cd0afac
Aspallar <shadowthrone99@outlook.com>
2022-08-25 21:08:06 +0100

    added tests for NotConverter +
    Made convert return true for non boolean values

---
+++ src/Tests/ValueConverterTests/NotConverterTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using WikiUpload;
+
+namespace Tests.ValueConverterTests
+{
+    [TestFixture]
+    public class NotConverterTests
+    {
+        private NotConverter _converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _converter = new NotConverter();
+        }
+
+        [Test]
+        public void When_True_Then_False()
+        {
+            var result = (bool)_converter.Convert(true, null, null, null);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void When_False_Then_True()
+        {
+            var result = (bool)_converter.Convert(false, null, null, null);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void When_NotBoolean_Then_True()
+        {
+            var result = (bool)_converter.Convert(this, null, null, null);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void When_Null_Then_True()
+        {
+            var result = (bool)_converter.Convert(null, null, null, null);
+            Assert.That(result, Is.True);
+        }
+
+    }
+}
