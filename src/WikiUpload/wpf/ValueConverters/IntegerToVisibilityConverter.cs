3f94a958e6b48c6ae8f49d0c445be59fb446f10f
Aspallar <shadowthrone99@outlook.com>
2022-09-20 12:35:06 +0100

    Rename (#22) Tidied popup layout and added error icon.

---
+++ src/WikiUpload/wpf/ValueConverters/IntegerToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace WikiUpload
+{
+    internal class IntegerToVisibilityConverter : BaseValueConverter<IntegerToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var intValue = value as int?;
+            return intValue.HasValue && intValue.Value > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
