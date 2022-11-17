e8a3b94514abd29c20693afe33d7788161d3249e
Aspallar <shadowthrone99@outlook.com>
2022-11-17 14:57:03 +0000

    Modified settings UI for inital values #34

---
+++ src/WikiUpload/Pages/SecrionHeader.xaml.cs
@@ -0,0 +1,63 @@
+﻿using MahApps.Metro.IconPacks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WikiUpload
+{
+    internal partial class SecrionHeader : UserControl
+    {
+        public SecrionHeader()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        #region Text Property
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                nameof(Text),
+                typeof(string),
+                typeof(SecrionHeader));
+
+        #endregion
+
+        #region IconKind Property
+
+        public PackIconFontAwesomeKind IconKind
+        {
+            get { return (PackIconFontAwesomeKind)GetValue(IconKindProperty); }
+            set { SetValue(IconKindProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconKindProperty =
+            DependencyProperty.Register(
+                nameof(IconKind),
+                typeof(PackIconFontAwesomeKind),
+                typeof(SecrionHeader));
+
+        #endregion
+
+        #region Subtext Property
+
+        public string Subtext
+        {
+            get { return (string)GetValue(SubtextProperty); }
+            set { SetValue(SubtextProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubtextProperty =
+            DependencyProperty.Register(
+                nameof(Subtext),
+                typeof(string),
+                typeof(SecrionHeader));
+
+        #endregion
+    }
+}
---
+++ src/WikiUpload/wpf/ValueConverters/StringToVisiblityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace WikiUpload
+{
+    internal class StringToVisiblityConverter : BaseValueConverter<StringToVisiblityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var shouldBeVisible = value is string str && str.Length > 0;
+            return shouldBeVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
